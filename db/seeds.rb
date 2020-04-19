eacf94a4e8e9517a0150e181d16f904c3d53882a
kelly_maass <kmaass77627@gmail.com>
2020-04-19 03:41:58 -0500

    seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+post_list = [
+  [ "Germany", "hey" ],
+  [ "France", "hi" ],
+  [ "Belgium", "hello" ],
+  [ "Netherlands", "yep" ]
+]
+
+post_list.each do |name, content|
+  Post.create( name: name, content: content)
+end
+
+record_list = [
+  [ "boo", "coo", "soo" ],
+  [ "loo", "too", "doo" ],
+  [ "moo", "roo", "eoo" ],
+  [ "zoo", "goo", "hoo" ]
+]
+
+record_list.each do |title, artist, genre|
+  Record.create(title: title, artist: artist, genre: genre)
+end
+
+user_list = [
+  [ "bop", "cop", "sop" ],
+  [ "lop", "top", "dop" ],
+  [ "mop", "rop", "eop" ],
+  [ "zop", "gop", "hop" ]
+]
+
+user_list.each do |username, email, password|
+  User.create( username: username, email: email, password: password)
+end
